027c36bc167e11bcdec17fe4cafdb18c67a23a2d
writedev <locxistwitch@gmail.com>
2025-05-25 12:38:33 +0200

    add a request for privilege in test/resgister

--- src/test/register.ps1
+++ src/test/register.ps1
@@ -1,5 +1,24 @@
 #Ordinateur\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons
 
+# Vérifie si le script est exécuté avec des droits d'administrateur
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    # Crée un nouvel objet StartInfo pour le processus PowerShell
+    $startInfo = New-Object System.Diagnostics.ProcessStartInfo
+    $startInfo.FileName = "powershell.exe"
+    $startInfo.Arguments = "-NoProfile -ExecutionPolicy Bypass -File `"$($MyInvocation.MyCommand.Path)`""
+    $startInfo.Verb = "runas" # Indique que le script doit être exécuté en tant qu'administrateur
+
+    # Lance le processus
+    try {
+        [System.Diagnostics.Process]::Start($startInfo)
+    } catch {
+        Write-Warning "Elevation of privileges failed. The script must be run as an administrator."
+    }
+
+    # Quitte le script actuel
+    exit
+}
+
 
 # Définir le chemin de la clé de registre
 $registryPath = "Ordinateur\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons"
@@ -15,3 +34,6 @@ $valueData = "MaDonnee"
 
 # Ajouter la valeur à la clé de registre
 New-ItemProperty -Path $registryPath -Name $valueName -Value $valueData -PropertyType String -Force
+
+Read-Host "Press Enter to exit..."
+exit
